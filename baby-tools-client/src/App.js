7a23910f9b01ac74066510f778876e39eb1ab97f
Badhree Babu <jbadhree@gmail.com>
2022-12-30 01:01:31 -0500

    adding time picker

--- baby-tools-client/src/App.js
+++ baby-tools-client/src/App.js
@@ -1,20 +1,30 @@
 import logo from './logo.svg';
 import './App.css';
-import { Button, Space } from 'antd';
+import { Button, Space, TimePicker, Divider, Row } from 'antd';
+import dayjs from 'dayjs';
+
+const format = 'HH:mm';
 
 function App() {
   return (
     <div className="App">
       <header className="App-header">
-        <Space wrap>
-          <Button type="primary">Start</Button>
-          <Button type="primary">Stop</Button>
-        </Space>
+          <Row>
+            <TimePicker defaultValue={dayjs('12:08', format)} format={format} />
+          </Row>
+          <Row><p></p></Row>
+          
+          <Row>
+
+            <Space>
+              <Button type="primary">Start</Button> 
+              <Button type="primary">Stop</Button>
+            </Space>
+          
+          </Row>
       </header>
     </div>
   );
 }
 
 export default App;
-
-
